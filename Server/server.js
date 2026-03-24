f9edc7a9e54e49d675a0dea4625cd115fb4f74dd
rajeshkanna <rajeshkannakbd@gmail.com>
2026-03-24 23:06:06 +0530

    polishing the ui

--- Server/server.js
+++ Server/server.js
@@ -11,7 +11,10 @@ const deliveryRoutes = require("./routes/DeliveryRoutes.js")
 
 const app = express();
 app.use(express.json());
-app.use(cors())
+app.use(cors({
+  origin: "https://deliverysaas-frontend.onrender.com/", 
+  credentials: true
+}));
 mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log("MongoDB connected"))
 .catch(err => console.log(err));
